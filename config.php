e229fa86cdf198936d93684a734ef52387e18247
Vitas Stradal <vitas@matfyz.cz>
2011-11-20 19:28:18 +0100

    config in separate file

---
+++ config.php
@@ -0,0 +1,45 @@
+<?
+/*
+ * configuration
+ */
+# where pdf files lives  
+$pdf_dir = '..';
+
+# where to store .png (must have write access)
+$img_dir = 'tmp';
+
+# where it is on web
+$img_uri = 'tmp';
+
+/*
+ * end of configuration
+ */
+$pdf_file = 'letak.pdf';
+
+/*
+ * connection string
+ */
+$db_conn = "sqlite:db/opraf.db";
+
+/*
+ * opraf.db:
+ *
+$ sqlite3 db/opaf.db
+
+CREATE TABLE opravy(
+	id INTEGER PRIMARY KEY,
+	pdf  TEXT,
+	img_id TEXT,
+	txt TEXT,
+	status TEXT,
+	au  TEXT,
+	x INTEGER,
+	y INTEGER
+);
+
+# set permissions 
+$ chomd o+w db/ db/opraf.db
+
+ *
+ */
+
