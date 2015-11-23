650768f0e6c39d5c6943b30d1266bdebecb512a2
Matěj Kocián <matej.kocian@gmail.com>
2015-11-23 19:02:51 +0100

    Odstraněno Google analytics

--- opraf.php
+++ opraf.php
@@ -813,17 +813,6 @@ function render_html($pdf_file, $au)
 	</form>
 	</div>
 	<?php  render_content() ?>
-	<!-- Google analytics -->
-	<script>
-  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- 	 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- 	 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
- 	 ga('create', 'UA-44119587-2', 'auto');
- 	 ga('send', 'pageview');
-
-	</script>
 </body> </html>
 <?php }
 
